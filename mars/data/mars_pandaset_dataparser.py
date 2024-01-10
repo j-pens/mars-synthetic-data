bf900574dfbfcd30edda42a7fb7c667a2b7c3a68
Jonas Penshorn <j.penshorn@tu-braunschweig.de>
2024-01-10 22:13:19 +0100

    Check cuboid dir

--- mars/data/mars_pandaset_dataparser.py
+++ mars/data/mars_pandaset_dataparser.py
@@ -995,6 +995,7 @@ class MarsPandasetParser(DataParser):
         print('seq dir:', seq._directory)
         seq.load_cuboids()
         CONSOLE.print(len(seq.cuboids.data))
+        CONSOLE.print(len(seq.cuboids._directory))
         visible_objects_, objects_meta_ = get_obj_pose_tracking_pandaset(seq.cuboids, self.selected_frames, np.eye(4), self.cameras_name_list, seq.camera, seq.lidar, self.coordinates_conversion)
             
         # # Align Axis with vkitti axis
