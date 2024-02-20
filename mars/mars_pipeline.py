c47c5ca7ecd93bc7452fee4184806362f255605c
Jonas Penshorn <j.penshorn@tu-braunschweig.de>
2024-02-20 20:18:54 +0100

    Adjusted to match code from memory fix PR

--- mars/mars_pipeline.py
+++ mars/mars_pipeline.py
@@ -215,18 +215,9 @@ class MarsPipeline(Pipeline):
                 # time this the following line
                 inner_start = time()
 
-                image_idx = batch["image_idx"]
+                object_rays_info = self.datamanager.eval_dataset.metadata["obj_info"][batch["image_idx"]]
 
-                object_rays_info = self.datamanager.eval_dataset.metadata["obj_info"][image_idx]
-
-                image_width = self.datamanager.eval_dataset.cameras[image_idx].width
-                image_height = self.datamanager.eval_dataset.cameras[image_idx].height
-                # print(f'Image size (HxW): {image_height[0]}x{image_width[0]}')
-
-                # object_rays_info = object_rays_info[None, ...].repeat_interleave(image_width[0], dim=0)
-                # object_rays_info = object_rays_info[None, ...].repeat_interleave(image_height[0], dim=0)
-
-                object_rays_info_view = object_rays_info.expand(image_height[0], image_width[0], -1, -1)
+                object_rays_info_view = object_rays_info.expand(camera_ray_bundle.shape[0], camera_ray_bundle.shape[1], -1, -1)
 
                 camera_ray_bundle.metadata["object_rays_info"] = object_rays_info_view.reshape(
                     camera_ray_bundle.shape[0], camera_ray_bundle.shape[1], -1
